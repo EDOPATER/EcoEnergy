07aa49645cd425bb3c7aff98d17823204e3bdc9d
EDOPATER <edopater@gmail.com>
2023-11-09 21:52:39 -0500

    Segunda Vesión

--- app/src/main/java/com/edopater/ecoenergy/MainActivity2.java
+++ app/src/main/java/com/edopater/ecoenergy/MainActivity2.java
@@ -43,16 +43,40 @@ public class MainActivity2 extends AppCompatActivity {
             public void onClick(View v) {
 
                 //Vamos a crear un mensaje con la información digitada por el user
-                String msj = "Nombre: " + eT1.getText().toString() +
+                /*String msj = "Nombre: " + eT1.getText().toString() +
                         "\nDirección: " + eT2.getText().toString() +
                         "\nTeléfono: " + eT3.getText().toString() +
                         "\nCorreo: " + eT4.getText().toString();
 
                 //Vamos a mostrarlo
-                Toast.makeText(getApplicationContext(), msj, Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(),msj,Toast.LENGTH_SHORT).show();*/
 
+                //----------------------------------------------------------------------------//
+                //Vamos a crear las condiciones para que me muestre de acuerdo al RadioButton
+                /*if (rB1.isChecked()) {
+                    Toast.makeText(getApplicationContext(),"Usted vive en una casa",Toast.LENGTH_SHORT).show();
+                }
+                if (rB2.isChecked()) {
+                    Toast.makeText(getApplicationContext(),"Usted vive en un apartamento",Toast.LENGTH_SHORT).show();
+                }
+                if (rB3.isChecked()) {
+                    Toast.makeText(getApplicationContext(),"Usted vive en zona rural",Toast.LENGTH_SHORT).show();
+                }*/
+
+                //----------------------------------------------------------------------------//
+                //Vamos a crear las condiciones para que me muestre de acuerdo al CheckBox
+                if (cB1.isChecked()) {
+                    Toast.makeText(getApplicationContext(),"Paneles Solares",Toast.LENGTH_SHORT).show();
+                }
+                if (cB2.isChecked()) {
+                    Toast.makeText(getApplicationContext(),"Aerogeneradores",Toast.LENGTH_SHORT).show();
+                }
+                if (cB3.isChecked()) {
+                    Toast.makeText(getApplicationContext(),"Otros",Toast.LENGTH_SHORT).show();
+                }
 
             }
         });
+
     }
 }
